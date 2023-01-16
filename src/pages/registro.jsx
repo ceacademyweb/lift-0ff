ce0e1c8127201d2d1ecfa23690fbefe5b2086bc2
Florentino Navarro <ceacademy.web@gmail.com>
2023-01-16 16:11:25 -0600

    copy

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -2,16 +2,41 @@ import { userState, useState } from 'react';
 import Logo from '../components/Logo';
 import addClass from '../utils/addClass';
 import Slider from './login/Slider';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, Navigate } from 'react-router-dom';
 import axios from 'axios';
+
+const resDrow = (name) => {
+  console.log('llega');
+  const modalRes = document.createElement('div');
+  modalRes.className = 'modal-res';
+  modalRes.innerHTML = `
+    <div class="modal-res__container">
+      <img src="/img/logo.svg" alt="" />
+      <h2>
+        !Gracias por tu registro ${name}!
+      </h2>
+      <p>
+        Estamos validado tus datos, si está todo eb orden racibiráns un correo con tu usuario y contraseña directo en tu email.
+      </p>
+      <a href="/">Cerrar</a>
+    </div>
+  `;
+  document.body.appendChild(modalRes);
+};
+
 const Registro = (e) => {
   addClass();
   const navigate = useNavigate();
   const [user, setUser] = useState({
     name: '',
     email: '',
-    password: '',
-    password_validator: '',
+    lastName: '',
+    email: '',
+    address: '',
+    discordId: '',
+    telegramId: '',
+    phone: '',
+    codeMember: '',
   });
   const submit = (e) => {
     e.preventDefault();
@@ -19,14 +44,18 @@ const Registro = (e) => {
     const formData = new FormData(target);
     setUser({
       name: formData.get('name'),
+      lastName: formData.get('lastName'),
       email: formData.get('email'),
-      password: formData.get('password'),
-      password_validator: formData.get('password_validator'),
+      address: formData.get('address'),
+      discordId: formData.get('discordId'),
+      telegramId: formData.get('telegramId'),
+      phone: formData.get('name'),
+      codeMember: formData.get('codeMember'),
     });
-    if (user.password_validator !== user.password) {
-      alert('Las contraseñs debe ser iguales');
-      return;
-    }
+    // if (user.password_validator !== user.password) {
+    //   alert('Las contraseñs debe ser iguales');
+    //   return;
+    // }
     const button = e.target.querySelector('button');
     button.innerHTML = '<img src="/img/load.svg" alt="">';
     // console.log(ajax('/login', dataForm, 'post'))
@@ -37,19 +66,21 @@ const Registro = (e) => {
         'content-type': 'application/json',
       },
     };
-    axios('https://ceacademy-auth-production.up.railway.app/users', options)
-      // axios('http://localhost:5000/login', options)
+    // axios('https://ceacademy-auth-production.up.railway.app/users', options)
+    axios('http://localhost:5000/users', options)
       .then((res) => {
+        console.log(res);
         button.innerHTML = 'correcto';
         if (res.status >= 200 && res.status < 300) {
-          navigate('/login');
+          // navigate('/login');
+          resDrow(user.name);
         }
         // const { decodedToken, isExpired, reEvaluateToken } = useJwt(res.data.token);
         // console.log(decodedToken)
         // navigate('/media');
       })
       .catch((err) => {
-        // e.target.querySelector('button').innerHTML='error'
+        e.target.querySelector('button').innerHTML = 'error';
         console.log(err);
       });
     console.log(user);
@@ -84,19 +115,30 @@ const Registro = (e) => {
             Inicia Sesión
           </NavLink>
         </div>
-        <h1>Registrate con Nosotros</h1>
         <form
           action=""
           className="form-section__form"
           autoComplete="off"
           onSubmit={submit}
         >
+          <h1>Registrate con Nosotros</h1>
           <div className="group">
             <label htmlFor="name">Nombre</label>
             <input
               type="text"
               name="name"
               id="name"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="group">
+            <label htmlFor="lastName">Apellido</label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
               placeholder=" "
               autoComplete="off"
               onChange={handleChange}
@@ -113,7 +155,76 @@ const Registro = (e) => {
               onChange={handleChange}
             />
           </div>
+
           <div className="group">
+            <label htmlFor="address">Domicilio</label>
+            <textarea
+              name="address"
+              id="address"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+              rows="4"
+            ></textarea>
+            {/* <input
+              type="text"
+              name="address"
+              id="address"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+            /> */}
+          </div>
+
+          <div className="group">
+            <label htmlFor="discordId">Usuario de discord</label>
+            <input
+              type="text"
+              name="discordId"
+              id="discordId"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="group">
+            <label htmlFor="telegramId">Usuario de telegram</label>
+            <input
+              type="text"
+              name="telegramId"
+              id="telegramId"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="group">
+            <label htmlFor="phone">Numero de contacto</label>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="group">
+            <label htmlFor="codeMember">Código de miembro</label>
+            <input
+              type="text"
+              name="codeMember"
+              id="codeMember"
+              placeholder=" "
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* <div className="group">
             <label htmlFor="password">Contraseña</label>
             <input
               type="password"
@@ -134,7 +245,7 @@ const Registro = (e) => {
               autoComplete="off"
               onChange={handleChange}
             />
-          </div>
+          </div> */}
           <div className="form-registro__footer">
             <div className="notes show"></div>
             <button className="register-btn">Registrar</button>
